719d68a9e5b8cd6021f120385f6f60beb046fd05
Tnalxmsk <alstnwkd990@naver.com>
2023-12-14 19:09:58 +0900

    feat(menu): 카테고리, 메뉴 추천 기능 추가

--- menu-tnalxmsk/src/main/kotlin/menu/RecommendationMenuApp.kt
+++ menu-tnalxmsk/src/main/kotlin/menu/RecommendationMenuApp.kt
@@ -1,6 +1,10 @@
 package menu
 
 import menu.model.Coach
+import menu.model.menu.Category
+import menu.model.menu.Week
+import menu.util.CategoryRecommender
+import menu.util.MenuRecommender
 import menu.view.InputView
 import menu.view.OutputView
 
@@ -11,6 +15,7 @@ class RecommendationMenuApp(
     fun startRecommendationMenu() {
         outputView.printServiceStart()
         val coaches = createCoaches()
+        val recommendationCategoryWithMenu = startRecommendation(coaches)
     }
 
     private fun createCoaches(): List<Coach> {
@@ -20,5 +25,26 @@ class RecommendationMenuApp(
             Coach(name, hateMenu)
         }
         return coaches
+    }
+
+    private fun startRecommendation(coaches: List<Coach>): List<MutableMap<String, out Any>> {
+        val recommendationCategory = mutableMapOf<String, String>()
+        val recommendationMenus = mutableMapOf<String, Map<String, String>>()
+        Week.entries.forEach { week ->
+            val category = CategoryRecommender.recommendCategory()
+            recommendationCategory[week.title] = category
+            recommendationMenus[week.title] = recommendMenu(category, coaches)
+        }
+        return listOf(recommendationCategory, recommendationMenus)
+    }
+
+    private fun recommendMenu(category: String, coaches: List<Coach>): Map<String, String> {
+        val coachesMenu = mutableMapOf<String, String>()
+        val menus = Category.getMenus(category)
+        coaches.forEach { coach ->
+            val menu = MenuRecommender.recommendMenu(menus)
+            coachesMenu[coach.name] = menu
+        }
+        return coachesMenu
     }
 }
