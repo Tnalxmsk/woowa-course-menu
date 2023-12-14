a9ea94eb0de1d10f7d8093c547ac792b6baf7fc4
Tnalxmsk <alstnwkd990@naver.com>
2023-12-14 20:33:55 +0900

    refactor(menu): 요일 형식 수정

--- menu-tnalxmsk/src/main/kotlin/menu/model/menu/Week.kt
+++ menu-tnalxmsk/src/main/kotlin/menu/model/menu/Week.kt
@@ -1,9 +1,9 @@
 package menu.model.menu
 
 enum class Week(val title: String) {
-    MON("월"),
-    TUE("화"),
-    WED("수"),
-    THU("목"),
-    FRI("금")
+    MON("월요일"),
+    TUE("화요일"),
+    WED("수요일"),
+    THU("목요일"),
+    FRI("금요일")
 }
