b009438c50da0816888a348b58809fd4e163eea4
Tnalxmsk <alstnwkd990@naver.com>
2023-12-14 17:14:51 +0900

    feat(menu): 메뉴 추천 앱 실행 세팅

---
+++ menu-tnalxmsk/src/main/kotlin/menu/RecommendationMenuApp.kt
@@ -0,0 +1,13 @@
+package menu
+
+import menu.view.InputView
+import menu.view.OutputView
+
+class RecommendationMenuApp(
+    private val inputView: InputView,
+    private val outputView: OutputView
+) {
+    fun startRecommendationMenu() {
+
+    }
+}
