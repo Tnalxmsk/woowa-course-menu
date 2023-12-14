686f39cab7ac7dc64bab769076c6249b3878698a
Tnalxmsk <alstnwkd990@naver.com>
2023-12-14 17:23:14 +0900

    feat(menu): 코치 이름 입력 기능 추가

--- menu-tnalxmsk/src/main/kotlin/menu/view/InputView.kt
+++ menu-tnalxmsk/src/main/kotlin/menu/view/InputView.kt
@@ -1,4 +1,17 @@
 package menu.view
 
+import camp.nextstep.edu.missionutils.Console
+
 class InputView {
+    fun raedCoachName(): List<String> {
+        println(INPUT_COACH_NAME)
+        val input = Console.readLine()
+        val names = input.split(NAME_SPLIT_DELIMITERS)
+        return names
+    }
+
+    companion object {
+        private const val INPUT_COACH_NAME = "코치의 이름을 입력해 주세요. (, 로 구분)"
+        private const val NAME_SPLIT_DELIMITERS = ","
+    }
 }
