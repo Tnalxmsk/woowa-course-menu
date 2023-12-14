8f122bca1f6bb4f564bd7524b3c43941da35991a
Tnalxmsk <alstnwkd990@naver.com>
2023-12-14 17:37:23 +0900

    feat(menu): 싫어하는 음식 입력 기능 추가

--- menu-tnalxmsk/src/main/kotlin/menu/view/InputView.kt
+++ menu-tnalxmsk/src/main/kotlin/menu/view/InputView.kt
@@ -6,12 +6,22 @@ class InputView {
     fun raedCoachName(): List<String> {
         println(INPUT_COACH_NAME)
         val input = Console.readLine()
-        val names = input.split(NAME_SPLIT_DELIMITERS)
+        val names = input.split(SPLIT_DELIMITERS)
         return names
+    }
+
+    fun readHateMenu(name: String): List<String> {
+        println(INPUT_COACH_WORST_MENU.format(name))
+        val input = Console.readLine()
+        println()
+
+        val menu = input.split(SPLIT_DELIMITERS)
+        return menu
     }
 
     companion object {
         private const val INPUT_COACH_NAME = "코치의 이름을 입력해 주세요. (, 로 구분)"
-        private const val NAME_SPLIT_DELIMITERS = ","
+        private const val INPUT_COACH_WORST_MENU = "%s(이)가 못 먹는 메뉴를 입력해 주세요."
+        private const val SPLIT_DELIMITERS = ","
     }
 }
